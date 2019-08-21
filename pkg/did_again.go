84ec8a59690b7f1d142d19c32165a30ac4a6a749
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 20:58:07 +0800

    update build line

--- pkg/did_again.go
+++ pkg/did_again.go
@@ -1,5 +1,9 @@
 // +build !nacl
+
 package pkg
+
+// GOARCH GOOS
+// amd64p32 nacl
 
 func TestNacl() string {
   return "normal"
--- pkg/did_again_nacl.go
+++ pkg/did_again_nacl.go
@@ -1,4 +1,5 @@
 // +build nacl
+
 package pkg
 
 func TestNacl() string {
