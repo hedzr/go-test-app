3027b7620a27afc6390ea6d592eea7cd9e971059
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 22:17:08 +0800

    for nacl testing

--- pkg/dids.go
+++ pkg/dids.go
@@ -1,4 +1,5 @@
-// +build linux,windows,freebsd,darwin,aix,arm_linux,netbsd,openbsd,plan9,solaris
+// +build darwin dragonfly freebsd linux netbsd openbsd windows aix arm_linux plan9 solaris
+// +build !nacl,!amd64p32
 
 package pkg
 
