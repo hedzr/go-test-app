46c12263fd586ed4ef5599a59ea52598069c1d88
Hedzr Yeh <hedzrz@gmail.com>
2020-04-11 16:25:38 +0800

    add peripheral files

---
+++ doc.go
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2019 Hedzr Yeh.
+ */
+
+/*
+
+Package tester is a demo app of mine, just for the study of new features
+
+*/
+package tester
+
+const (
+	// AppName const
+	AppName = "tester"
+	// Version const
+	Version = "1.6.32"
+	// VersionInt const
+	VersionInt = 0x010620
+)
